839f316a245c5f4e9a00285695ddb17c1924d2dc
ddoberne <ddoberne@gmail.com>
2024-01-21 19:32:49 -0800

    Update display.py

--- display.py
+++ display.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import event
 
 gem_bundle_prices = ("$4.99", "$9.99", "$19.99", "$49.99", "$99.99")
 gem_bundle_values = (750.0/5, 1600.0/10, 3400.0/20, 9200.0/50, 20000.0/100)
@@ -9,4 +10,7 @@ for i in range(0, len(gem_bundle_prices)):
 user_bundle = st.sidebar.selectbox('Which bundle do you purchase?', gem_bundle_prices)
 user_gems_per_usd = gems_per_usd[user_bundle]
 st.sidebar.write(f'Your gems are worth {user_gems_per_usd:.2f} gems per dollar.')
-st.sidebar.slider(label = 'Select game winrate:', min_value = 0.00, max_value = 1.00, step = 0.01)
+user_winrate = st.sidebar.slider(label = 'Select game winrate:', min_value = 0.00, max_value = 1.00, step = 0.01)
+st.sidebar.write(f'You win {(user_winrate ** 2) + (2 * user_winrate * user_winrate * (1 - user_winrate):.2f} of your Bo3s.')
+
+
